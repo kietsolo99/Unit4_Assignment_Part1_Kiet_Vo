73eaf179188e5ed0db41da9f0caeb1cc0b02a6d6
kietsolo99 <kiet.vo054@my.tccd.edu>
2020-02-03 18:11:39 +0000

    Create a C++ Program to accept input from the user while the value entered does not equal -1

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 	and/or legitimate user error. 
 	//Try and Catch blocks are used in error checking
 	Teacher: Dr. Tyson McMillan, Ph.D.  
-  STUDENT (McMillanite):
+  STUDENT (KietVo9): Unit_4_Assignment
 */
 #include <cstdlib>  // Provides EXIT_SUCCESS
 #include <iostream>  // Provides cout, cerr, endl
@@ -19,90 +19,34 @@ using namespace std;
 
 int main()
 {
-   //***********Validating int data type******************/
-	int input = 0;
-	input = validateInt(input); //validate my integer by function call, save that value into input
-    cout << "You entered valid int: " << input << endl;
-    
-	/****************Extend code to Validate double (follow int pattern)**********/
-	double inputDouble = 0.0;
-  	inputDouble = validateDouble(inputDouble); //validate my double by function call, save that value into inputDouble
-    cout << "You entered valid double: " << inputDouble << endl;
-
-	/****************Extend code to Validate char (follow int pattern)**********/
-	char inputChar = '\0'; //{0}
-	inputChar = validateChar(inputChar); //validate my char by function call, save that value into inputChar
-    cout << "You entered valid char: " << inputChar << endl;
-
-	/****************Extend code to Validate string (follow int pattern, if possible)**********/
-	string inputString = (""); 
-	inputString = validateString(inputString); //validate my string by function call, save that value into inputString
-    cout << "You entered string: " << inputString << endl;
-
-	/****************Extend code to Validate if truly an int and greater than 77 **********/
-	int input2 = 0;
-	
-	do
-	{
-		cout << "Enter an integer greater than 77." << endl;  
-		input2 = validateInt(input2); //validate my integer by function call, save that value into input
-		
-		if(input2 < 77)
-		{
-			cout << "\nInvalid input: (" << input2 <<") number must be greater than 77." << endl;
-		}
-	}
-	while(input2 < 77); //loop until input2 is greather than 77
-
-    cout << "You entered valid int: " << input2 << endl;
-
-	/****************Extend code to Validate if truly an double and greater than 0.00 **********/
-	double inputDouble2 = 0.0;
-	
-	do
-	{
-		cout << "Enter an number greater than 0."<< endl; 
-		inputDouble2 = validateDouble(inputDouble2); //validate my integer by function call, save that value into input
-		
-		if(inputDouble2 < 0)
-		{
-			cout << "\nInvalid input: (" << inputDouble2 <<") number must be greater than 0." << endl;
-		}
-	}
-	while(inputDouble2 < 0); //loop until inputDouble2 is greather than 0
-
-    cout << "You entered valid double: " << inputDouble2<< endl;
-    //A program to output grade values
-   //based upon numberical input of the user yourName: Teacher: Dr_T 8-28-2019
-   double grade = 0.0; 
-   do
-   { // begin do 
-      cout << "\nPlease enter a numerical grade (-1 to exit): ";
-      grade = validateDouble(grade); //accept grade and validate the data type
-      //if condition test
-      if(grade >= 90.0)
+  double n = 0.0, timeItself = 0.0, divideItself =0.0, output1 = 0.0, output2 = 0.0;//Declare and initialize variable
+  do
+  {
+    cout <<"Please enter a number (-1 to exit): ";
+        n = validateDouble(n);//accept number and validate data type
+    if (n == -1.0)
       {
-        cout << "\nThat's an A!" << endl; 
+        cout << "\nExit the program... " << endl;
       }
-      else if(grade >= 80.0 && grade <= 89.9)
-      {
-        cout << "\nThat's a B!" << endl; 
-      }
-      else if(grade >= 70.0 && grade <= 79.9)
-      {
-        cout << "\nThat's a C!" << endl; 
-      }
-      else if(grade >= 60.0 && grade <= 69.9)
-      {
-        cout << "\nYikes. That's a D!" << endl; 
-      }
-      else if(grade < 60.0)
-      {
-        cout << "\nSee you next semester: F" << endl; 
-      }
-      else {cout << "\nInvalid input." << endl;}
+      else
+      { 
+        //process
+        timeItself = n * n;
+        divideItself = n / n; // if n = 0 the result will be -nan
+        output1 = (n + 3) / 5;
+        output2 = ((n + 3) / 5 + (n + 7) / 2);
 
-   }while(grade != -1); //end do 
+        //output
+        cout <<"\nThe number entered times itself: " <<"n"<<" * "<<"n"<<" = "<< timeItself << endl;
+        cout <<"The number entered divides itself: " <<"n"<<" / "<<"n"<<" = "<< divideItself << endl;
+        cout << "(" <<" n "<<" + "<<" 3 "<<") "<<" / "<<" 5 "<< " = " << output1 <<endl;
+        cout <<"(" <<" n "<<" + "<<" 3 "<<") "<<" / "<<" 5 "<<" + "<<" (" <<" n "<<" + "<<" 7 "<<") "<<" / "<<" 2 "<<" = " << output2 << endl;
+      }
+
+  }while(n != -1); //end do
+
+  //Couter variable
+  
    
     return 0;
 }
